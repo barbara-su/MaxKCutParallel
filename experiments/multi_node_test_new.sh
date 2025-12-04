4ff1a12f82bfff91868cb1c24b44b98172747930
Barbara Su <bs82@login1.range.rice.edu>
2025-12-04 17:39:07 -0600

    symmetric run

--- experiments/multi_node_test_new.sh
+++ experiments/multi_node_test_new.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=multi-nodes-test
-#SBATCH --output=logs/multi-nodes-test-%j.out
-#SBATCH --error=logs/multi-nodes-test-%j.err
+#SBATCH --output=logs/multi-nodes-test-new-%j.out
+#SBATCH --error=logs/multi-nodes-test-new-%j.err
 #SBATCH --nodes=2
 #SBATCH --tasks-per-node=1
 
 #SBATCH --partition=commons
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=8GB
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=16GB
 #SBATCH --time=23:00:00
 
 # Clean environment
@@ -31,13 +31,13 @@ port=6379
 head_ip=$(srun -N1 -n1 -w "$head_node" hostname -I | awk '{print $1}')
 
 ip_head="${head_ip}:${port}"
-export ip_head
+export ip_heada
 echo "Ray head GCS address: $ip_head"
 
 srun --nodes="$SLURM_JOB_NUM_NODES" --ntasks="$SLURM_JOB_NUM_NODES" \
-    python -m read_only.symmetric_run \
-    --address "$ip_head" \
-    --min-nodes "$SLURM_JOB_NUM_NODES" \
-    --num-cpus="${SLURM_CPUS_PER_TASK}" \
-    -- \
-    python -u src/test_multi_nodes.py
+    ray symmetric-run \
+        --address "$ip_head" \
+        --min-nodes "$SLURM_JOB_NUM_NODES" \
+        --num-cpus "${SLURM_CPUS_PER_TASK}" \
+        -- \
+        python -u src/test_multi_nodes.py
